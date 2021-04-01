e096d7cb53bb5260542496a1960c97e281fcae9c
danielj0nes <>
2021-04-01 22:12:01 +0100

    Initial basic menu configuration

--- app/src/main/java/com/example/priisvergliich/MainActivity.java
+++ app/src/main/java/com/example/priisvergliich/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.priisvergliich;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.pvg_menu0, menu);
+        return true;
     }
 }
