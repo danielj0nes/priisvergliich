946a277fcc67181ebcc4abe946b556555f8b0382
danielj0nes <>
2021-04-06 00:03:03 +0100

    Complete implementation of the search bar skeleton functionality
    This will need later work to actually search for products for the comparisons

--- app/src/main/java/com/example/priisvergliich/MainActivity.java
+++ app/src/main/java/com/example/priisvergliich/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.priisvergliich;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +22,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.pvg_menu0, menu);
+        MenuItem.OnActionExpandListener onActionExpandListener=new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
 
-
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                return true;
+            }
+        };
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search for a product...");
         return true;
     }
 }
